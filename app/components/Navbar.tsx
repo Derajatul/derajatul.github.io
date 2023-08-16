dd21cda40422c614730bbd249b6aaad7c2c98373
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-16 08:00:00 +0000

    add navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return(
+    <>
+      <a href='#home' className="hover:text-white">home</a>
+      <a href='#stack' className="hover:text-white">stack</a>
+      <a href='#projects' className="hover:text-white">projects</a>
+    </>
+  )
+}
+export default Navbar
