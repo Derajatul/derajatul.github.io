2874035587c3c7a1b968e58ff8452e44aad74e94
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-30 07:17:02 +0000

    add footer components ✅

---
+++ app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const Footer = () => {
+  return(
+    <footer className="h-36 flex items-center">
+      <Link href='https://github.com/Derajatul'>Github</Link>
+    </footer>
+  )
+}
+
+export default Footer
