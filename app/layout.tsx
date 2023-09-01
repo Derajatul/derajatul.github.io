e0898b218cb9596780f1dd09200598f34518229a
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-01 09:41:18 +0000

    update page ✅

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>   
         <Navbar /> 
-        <main>
+        <main className="min-h-screen">
           {children}
         </main>
         <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,48 @@
 import Badge from './components/Badge'
 import projects from './dataProjects.json'
 import Image from 'next/image'
+import ProjectCard from './components/ProjectCard'
 
 interface Skill {
   img: string;
   alt: string;
+  link: string;
 }
 
 const SkillList: React.FC = () => {
   const skills: Skill[] = [
     {
-      img: '/icons/javascript.svg',
-      alt: 'javascript'
+      img: '/icons/typescript.svg',
+      alt: 'typescript',
+      link: 'https://www.typescriptlang.org/'
     },
     {
       img: '/icons/react.svg',
-      alt: 'react'
+      alt: 'react',
+      link: 'https://react.dev/'
     },
     {
       img: '/icons/nextjs.svg',
-      alt: 'nextjs'
+      alt: 'nextjs',
+      link: 'https://nextjs.org/'
+    },
+    {
+      img: '/icons/tailwindcss.svg',
+      alt: 'tailwindcss',
+      link: 'https://tailwindcss.com/'
     },
     {
       img: '/icons/jest.svg',
-      alt: 'jest'
-    }
+      alt: 'jest',
+      link: 'https://jestjs.io/'
+    },
+    
   ];
 
   return (
     <div className="flex flex-wrap gap-2 py-2">
       {skills.map((skill: Skill) => (
-        <Badge key={skill.alt}>
+        <Badge key={skill.alt} href={skill.link}>
           <Image src={skill.img} alt={skill.alt} width={48} height={48} />
         </Badge>
       ))}
@@ -40,12 +52,13 @@ const SkillList: React.FC = () => {
 
 const ProjectList = () => {
   return(
-    <div className="md:flex md:flex-wrap md:justify-between">
+    <div className="flex flex-wrap gap-3 justify-between">
       {projects.map(project => ( 
-        <div className="py-2 md:w-[48%]" key={project.id}>
-          <img src={project.img} />
-          <h3 className="py-5">{project.name} - {project.description}</h3>
-        </div>
+        <ProjectCard 
+          key={project.id}
+          img={project.img} 
+          name={project.name}
+          description={project.description} />
       ))}
     </div> 
   )
@@ -56,8 +69,14 @@ export default function Home() {
     <>
       <section id="home">
         <h1 className="text-xl font-bold py-5">halo, nama saya Dera 👋</h1>
-        <p className="py-2">Seorang front-end developer dengan fokus pada pengembangan web modern menggunakan teknologi <Badge><img src="/icons/react.svg" className="h-3 px-1" /> React</Badge> dan <Badge><img src="/icons/nextjs.svg" className="h-3 px-1"/> Next.js</Badge>. Saya memiliki tekad untuk menciptakan antarmuka pengguna yang menarik, responsif, dan berkinerja tinggi.</p>
-        <p className="py-2">Telah lulus sebagai Front-end Expert dari <Badge><img src="/icons/dicoding.jpeg" className="h-3 px-1"/>Dicoding</Badge>, menguasai konsep dan praktik terbaik dalam pengembangan front-end.</p>
+        <p className="py-2">Seorang front-end developer dengan fokus pada pengembangan web modern menggunakan teknologi
+        <Badge href='https://react.dev/'><img src="/icons/react.svg" className="h-3 px-1" /> React</Badge>
+          dan 
+        <Badge href="https://nextjs.org/"><img src="/icons/nextjs.svg" className="h-3 px-1"/> Next.js</Badge>
+          . Saya memiliki tekad untuk menciptakan antarmuka pengguna yang menarik, responsif, dan berkinerja tinggi.</p>
+        <p className="py-2">Telah lulus sebagai Front-end Expert dari 
+        <Badge href="https://www.dicoding.com/"><img src="/icons/dicoding.jpeg" className="h-3 px-1"/>Dicoding</Badge>
+          , menguasai konsep dan praktik terbaik dalam pengembangan front-end.</p>
       </section>
       
       <section id="stack">
