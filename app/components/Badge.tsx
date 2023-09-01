87fdd7396ae165a4d1109a8a7b2994c74449aae5
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-01 09:39:31 +0000

    update components ✅

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -1,10 +1,19 @@
-const Badge = (props) => {
+import Link from 'next/link'
+
+type Badge = {
+  children: React.ReactNode;
+  href: string;
+}
+
+const Badge = ({children,href}:Badge) => {
   return (
-    <a
-      {...props}
+    <Link
+      href={href}
       target="_blank"
       className="border border-neutral-700 bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-100"
-    />
+    >
+      {children}
+    </Link>
   );
 }
 export default Badge
