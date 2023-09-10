f4523f8fc6ca4a1ffa304b64541a28c8a8ce11e4
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-10 22:09:35 +0000

    update project button ✅

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -9,9 +9,15 @@ type ProjectCard = {
   code: string;
 }
 
-const Button = ({children,href}:{children: React.ReactNode,href:string}) => {
+const Button = ({children,href,style}:{children: React.ReactNode,href:string,style:string}) => {
+
+  let styleBtn = ''
+  if(style === 'solid'){
+    styleBtn = 'bg-neutral-800'
+  }
+  const buttonStyle=`${styleBtn} px-2 py-1 border border-neutral-700 rounded h-[44px] text-sm inline-flex items-center leading-4 text-neutral-100 justify-center`
   return(
-    <Link href={href} target="_blank" className="px-2 py-1 border border-neutral-700 bg-neutral-800 rounded h-[44px] text-sm inline-flex items-center leading-4 text-neutral-100 justify-center">{children}</Link>
+    <Link href={href} target="_blank" className={buttonStyle}>{children}</Link>
   )
 }
 
@@ -21,8 +27,8 @@ const ProjectCard = ({img,name,description,url,code}: ProjectCard) => {
       <Image src={img} alt='' width={500} height={500} style={{ maxWidth: '100%', height: 'auto'}} className="hover:shadow-inner" />
       <h3 className="my-5">{name} - {description}</h3>
       <div className="flex flex-wrap gap-2">
-        <Button href={url}>preview site &#8599;</Button>
-        <Button href={code}>view code &#8599;</Button>
+        <Button style="solid" href={url}>preview site &#8599;</Button>
+        <Button style="outline" href={code}>view code &#8599;</Button>
       </div>
       
       
